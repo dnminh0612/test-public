c13300d1fcc2c78dcfe2c3ff7fe71d4924d199b7
NHAT MINH <vitop.minhdoan@gmail.com>
2019-09-22 17:13:07 +0700

    213

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Products;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+       dd( Products::all());
         return view('home');
     }
 }
