94c72a44b19cb1f86ed61afb45f093b2f75936f3
NHAT MINH <vitop.minhdoan@gmail.com>
2019-09-22 17:36:08 +0700

    123

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,12 +23,12 @@
     <div class="row">
         @foreach($products as $product)
             <div class="col-12 col-lg-4">
-                <div class="card">
-                    <img src="{{ Storage::url($product->image) }}" class="card-img-top" alt="...">
+                <div class="card mt-3">
+                    <img src="{{ Storage::url($product->image) }}" class="card-img-top w-100" alt="{{$product->name}}">
                     <div class="card-body">
                         <h5 class="card-title">{{$product->name}}</h5>
                         <p class="card-text">{{$product->description}}</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <a href="#" class="btn btn-primary">View</a>
                     </div>
                 </div>
             </div>
